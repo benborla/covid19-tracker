b7e8128866de48007cae51b0d7476d276def3246
Ben Borla <benborla@Bens-MacBook-Pro.local>
2020-04-19 01:48:34 +0800

    added material ui package

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import { Covid19 } from './features/covid19/Covid19'
 import './App.css';
+import Button from '@material-ui/core/Button'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <Covid19 />
+             <Button variant="contained" color="primary">
+      Hello World
+    </Button>
       </header>
     </div>
   );
